6a1ecadcc22a931dd52e1f7777d438f5c681665d
novvan <ianallengeier@gmail.com>
2020-08-25 16:48:27 -0300

    part 12

--- test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
+++ test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
@@ -1,3 +1,5 @@
+import 'package:cleanflutter/core/error/failures.dart';
+import 'package:cleanflutter/core/usecases/usecase.dart';
 import 'package:cleanflutter/core/util/input_converter.dart';
 import 'package:cleanflutter/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:cleanflutter/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
@@ -41,17 +43,53 @@ void main() {
     final tNumberParsed = 1;
     final tNumberTrivia = NumberTrivia(text: 'Test Trivia', number: 1);
 
+    void setUpMockInputConverterSuccess() {
+      when(mockInputConverter.stringToUnsignedInteger(any))
+          .thenReturn(Right(tNumberParsed));
+    }
+
     test(
       'Should call the InputConverter to validate and convert the string',
       () async {
         //arrange
-        when(mockInputConverter.stringToUnsignedInteger(any))
-            .thenReturn(Right(tNumberParsed));
+        setUpMockInputConverterSuccess();
         //act
         bloc.dispatch(GetTriviaForConcreteNumber(tNumberString));
         await untilCalled(mockInputConverter.stringToUnsignedInteger(any));
         //assert
         verify(mockInputConverter.stringToUnsignedInteger(tNumberString));
+      },
+    );
+    test(
+      'Should get data from the concrete use case',
+      () async {
+        //arrange
+        setUpMockInputConverterSuccess();
+        when(mockGetConcreteNumberTrivia(any))
+            .thenAnswer((_) async => Right(tNumberTrivia));
+        //act
+        bloc.dispatch(GetTriviaForConcreteNumber(tNumberString));
+        await untilCalled(mockGetConcreteNumberTrivia(any));
+        //assert
+        verify(mockGetConcreteNumberTrivia(Params(number: tNumberParsed)));
+      },
+    );
+    test(
+      'Should emit [Loading, Loaded] when data is gotten successfully',
+      () async {
+        //arrange
+        setUpMockInputConverterSuccess();
+        when(mockGetConcreteNumberTrivia(any))
+            .thenAnswer((_) async => Right(tNumberTrivia));
+        //assert later
+        final expectedList = [
+          Empty(),
+          Loading(),
+          Loaded(trivia: tNumberTrivia),
+        ];
+        expectLater(bloc.state, emitsInOrder(expectedList));
+        //act
+        bloc.dispatch(GetTriviaForConcreteNumber(tNumberString));
       },
     );
     test(
@@ -71,6 +109,111 @@ void main() {
         );
         //act
         bloc.dispatch(GetTriviaForConcreteNumber(tNumberString));
+      },
+    );
+    test(
+      'Should emit [Loading, Error] when there was an error retrieving SERVER data',
+      () async {
+        //arrange
+        setUpMockInputConverterSuccess();
+        when(mockGetConcreteNumberTrivia(any))
+            .thenAnswer((_) async => Left(ServerFailure()));
+        //assert later
+        final expectedList = [
+          Empty(),
+          Loading(),
+          Error(message: SERVER_FAILURE_MESSAGE),
+        ];
+        expectLater(bloc.state, emitsInOrder(expectedList));
+        //act
+        bloc.dispatch(GetTriviaForConcreteNumber(tNumberString));
+      },
+    );
+    test(
+      'Should emit [Loading, Error] when there was an error retrieving CACHED data',
+      () async {
+        //arrange
+        setUpMockInputConverterSuccess();
+        when(mockGetConcreteNumberTrivia(any))
+            .thenAnswer((_) async => Left(CacheFailure()));
+        //assert later
+        final expectedList = [
+          Empty(),
+          Loading(),
+          Error(message: CACHE_FAILURE_MESSAGE),
+        ];
+        expectLater(bloc.state, emitsInOrder(expectedList));
+        //act
+        bloc.dispatch(GetTriviaForConcreteNumber(tNumberString));
+      },
+    );
+  });
+
+  group('getTriviaForRandomNumber', () {
+    final tNumberTrivia = NumberTrivia(text: 'Test Trivia', number: 1);
+
+    test(
+      'Should get data from the random use case',
+      () async {
+        //arrange
+        when(mockGetRandomNumberTrivia(any))
+            .thenAnswer((_) async => Right(tNumberTrivia));
+        //act
+        bloc.dispatch(GetTriviaForRandomNumber());
+        await untilCalled(mockGetRandomNumberTrivia(any));
+        //assert
+        verify(mockGetRandomNumberTrivia(NoParams()));
+      },
+    );
+    test(
+      'Should emit [Loading, Loaded] when data is gotten successfully',
+      () async {
+        //arrange
+        when(mockGetRandomNumberTrivia(any))
+            .thenAnswer((_) async => Right(tNumberTrivia));
+        //assert later
+        final expectedList = [
+          Empty(),
+          Loading(),
+          Loaded(trivia: tNumberTrivia),
+        ];
+        expectLater(bloc.state, emitsInOrder(expectedList));
+        //act
+        bloc.dispatch(GetTriviaForRandomNumber());
+      },
+    );
+    test(
+      'Should emit [Loading, Error] when there was an error retrieving SERVER data',
+      () async {
+        //arrange
+        when(mockGetRandomNumberTrivia(any))
+            .thenAnswer((_) async => Left(ServerFailure()));
+        //assert later
+        final expectedList = [
+          Empty(),
+          Loading(),
+          Error(message: SERVER_FAILURE_MESSAGE),
+        ];
+        expectLater(bloc.state, emitsInOrder(expectedList));
+        //act
+        bloc.dispatch(GetTriviaForRandomNumber());
+      },
+    );
+    test(
+      'Should emit [Loading, Error] when there was an error retrieving CACHED data',
+      () async {
+        //arrange
+        when(mockGetRandomNumberTrivia(any))
+            .thenAnswer((_) async => Left(CacheFailure()));
+        //assert later
+        final expectedList = [
+          Empty(),
+          Loading(),
+          Error(message: CACHE_FAILURE_MESSAGE),
+        ];
+        expectLater(bloc.state, emitsInOrder(expectedList));
+        //act
+        bloc.dispatch(GetTriviaForRandomNumber());
       },
     );
   });
